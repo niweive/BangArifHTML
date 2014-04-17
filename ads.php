27cb92e919cf2710bad15fe3a93aa053b02e97da
niweive <erwins@gmail.com>
2014-04-17 16:59:40 +0700

    Update Ads

--- ads.php
+++ ads.php
@@ -24,7 +24,7 @@
 	        	
 	        	<h2>
 	        				<b>Pasang Iklan di BangArif.com</b><br>
-	        					This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+	        					Pasang iklan
 	        					<br><br>
 	        					<b>Apa sih untungnya?</b><br>
 	        					Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. <br><br>
